2620de402899d21c50db7f53d238d1a94dca198d
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-08-07 23:10:49 -0400

    api key not working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import React from 'react';
+import {SearchBar, VideoDetail, VideoList} from './components';
+import youtube from './api/youtube';
 
 import { Grid } from '@material-ui/core'
 
 class App extends React.Component {
+
+  
+    //stops execution of the code until something is fetched
+    handleSubmit =  async (searchTerm) =>{
+        const response = await youtube.get('search',  {
+        
+        
+        params:  {
+            part: 'snippet',
+            maxResults: 5,
+            q: searchTerm,
+        }
+        
+
+        });
+
+        console.log(response)
+
+    }
+
+
+
     render() {
         return (
 
 <Grid style={{justifyContent: 'center'}} container spacing={10}>
-<Grid item xs={11}>
+<Grid item xs={12}>
 <Grid container spacing={10}>
 <Grid item xs={12}>
     {/* SearchBar Component */}
+    {/* transfering state to app.js */}
+    <SearchBar onFormSubmit={this.handleSubmit} />
     </Grid>
     <Grid item xs={8}>
         {/* VideoDetail omponent */}
+        <VideoDetail />
 </Grid>
     <Grid item xs={4}>
         {/* This is where VideoList component */}
+        <VideoList />
 </Grid>
 </Grid>
 </Grid>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import {Paper, TextField} from '@material-ui/core';
 
 class SearchBar extends React.Component{
+    state = {
+        searchTerm: ""
+    }
+
+    // need 'this' here refers to the class SearchBar
+    // setState what the current state of the property in the state object
+    // this function will update our search term in the state
+    handleChange = (event) => {this.setState({searchTerm: event.target.value })}
+
+//destructuring - takes in the search term from this.state
+    handleSubmit = (event) => {
+        const {searchTerm} = this.state;
+        const {onFormSubmit} = this.props;
+
+        onFormSubmit(searchTerm)
+
+        event.preventDefault();
+    }
+
     render(){
-        return (<h1>Search</h1>)
+        const {searchTerm} = this.state
+     return (
+        <Paper elevation={6} style={{padding: '25px'}}>
+            <form onSubmit={this.handeSubmit}>
+                <TextField fullWidth label="Search..." value={searchTerm} onChange={this.handleChange} />
+                </form>
+            </Paper>
+        
+        
+        
+        );
     }
 
 
